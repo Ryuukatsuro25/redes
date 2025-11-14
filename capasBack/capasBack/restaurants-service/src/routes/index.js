6b89aca00992af03aaaf0f1fb534bb7a2320e200
RyuuKatsuro <123609042+Ryuukatsuro25@users.noreply.github.com>
2025-11-13 23:08:46 -0500

    a

--- capasBack/capasBack/restaurants-service/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-                import { Router } from 'express';
-                import * as ctl from '../controllers/restaurants.controller.js';
-                const r = Router();
-                r.get('/', ctl.list);
-r.get('/:id', ctl.getOne);
-r.post('/', ctl.create);
-
-                export default r;
--- capasBack/capasBack/users-service/src/routes/index.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import * as ctl from '../controllers/users.controller.js';
-const r = Router();
-r.get('/me', ctl.me);
-
-export default r;
